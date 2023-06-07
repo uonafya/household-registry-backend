bb77fb539dc417e8970af5002e292827012e99a6
samAricha <samaricha001.com>
2023-06-07 13:26:06 +0300

    added house hold type model

---
+++ database/migrations/2023_06_07_075438_create_house_hold_types_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('house_hold_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('household_type_id');
+            $table->string('household_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('house_hold_types');
+    }
+};
