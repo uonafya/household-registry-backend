d462ba3349c8f0824744030577fb4e276e2557a0
agesa3 <collinsagesa2000@gmail.com>
2023-06-30 12:29:25 +0300

    able to migrate members and void the seconmd household

--- app/Http/Controllers/HouseHoldController.php
+++ app/Http/Controllers/HouseHoldController.php
@@ -427,50 +427,47 @@ class HouseHoldController extends Controller
         }
     }
 
-    public function mergeHouseHolds(Request $request){
-        try{
+    public function mergeHouseHolds(Request $request)
+    {
+        try {
 
             $request->validate([
                 'household_id_one' => 'required|integer',
                 'household_id_two' => 'required|integer',
-                'merging_initaited_by'=> 'required|integer',
-                'merging_approved_by'=> 'required|integer',
+                'merging_initaited_by' => 'required|integer',
+                'merging_approved_by' => 'required|integer',
             ]);
 
             //find the two households by id and get there members
-            $householdOne=HouseHold::where('id',$request->input('household_id_one'))->first();
-            $householdTwo= HouseHold::where('id',$request->input('household_id_two'))->first();
+            $householdOne = HouseHold::where('id', $request->input('household_id_one'))->first();
+            $householdTwo = HouseHold::where('id', $request->input('household_id_two'))->first();
 
-            if(!$householdOne || !$householdTwo){
+            if (!$householdOne || !$householdTwo) {
                 return response()->json([
-                    'data'=> 'Household Members not found'
-                ],200);
+                    'data' => 'Household Members not found'
+                ], 200);
             }
 
             $householdTwoMembers = $householdTwo->household_memberships;
 
+            foreach ($householdTwoMembers as $membership) {
+                $membership->house_hold_id = $householdOne->id;
+                $membership->save();
+            }
+            
+            // Void household two
+            $householdTwo->delete();
+
+            // Get the updated members of household one
+            $householdOneMembers = $householdOne->household_memberships;
+
             return response()->json([
                 'success' => true,
-                'message' => $householdTwoMembers,
+                'data' => [
+                    'householdOneMembers' => $householdOneMembers,
+                ],
             ], 200);
-
-            
-            // 
-
-
-            //now take the household members of household two and then change their memberType from household id of two to household id of one here is how the model of household membertype looks like
-            //    $householdMemberType = HouseholdMemberType::where('id', $memberData['household_member_type_id'])->first();
-            // $householdMembership = HouseHoldMembership::create([
-            //     'household_person_details_id' => $member->id,
-            //     'household_member_type_id' => $householdMemberType->id,
-            //     'house_hold_id' => $household->id,
-            // ]);
-            //loop through the mebers of household two and change their household membeship from household two to one
-            //then void household two 
-
-
-
-        }catch(\Throwable $e){
+        } catch (\Throwable $e) {
             return response()->json([
                 'success' => false,
                 'message' => 'Failed to merge households',
